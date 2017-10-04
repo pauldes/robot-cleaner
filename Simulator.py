032502688684725a04a7cdba9a286b9f1e142d76
pauldes <paul.desigaud@insa-lyon.fr>
2017-10-05 01:41:12 +0200

    rewards,actions

--- Simulator.py
+++ Simulator.py
@@ -3,33 +3,61 @@ import State
 
 #########################################
 
-def robotIsOnBase(s):
+# Robot is at the charging base
+def robotIsAtBase(s):
   return s.posRobot==s.posBase
-
-def wallUp(s):
-  return True
-
-def wallDown(s):
-  return True
-
-def wallRight(s):
-  return True
-
+# Walls surrounding the robot
 def wallLeft(s):
-  return True
+  return s.posRobot[0]==0
+def wallRight(s):
+  return s.posRobot[0]==s.sizeX-1
+def wallTop(s):
+  return s.posRobot[1]==0
+def wallBottom(s):
+  return s.posRobot[1]==s.sizeY-1
+# Current cell
+def currentCellIsDirty(s):
+  return 1==s.roomGrid[s.posRobot[1]][s.posRobot[0]]
+
+# Actions
+action_move_left = 'MOVE_LEFT'
+action_move_right = 'MOVE_RIGHT'
+action_move_up = 'MOVE_UP'
+action_move_down = 'MOVE_DOWN'
+action_vacuum = "VACUUM"
+action_recharge = "RECHARGE"
+action_move = "MOVE?"
+
+# Rewards
+reward_battery_empty = {action_vacuum : -20, action_move : -20, action_recharge : 10}
+reward_battery_full = {action_vacuum : -1, action_move : -1, action_recharge : -10}
+reward_cell_clean = {action_vacuum : -1, action_move : -1, action_recharge : 0}
+reward_cell_dirty = {action_vacuum : 5, action_move : -1, action_recharge : 0}
+reward_battery_empty = {action_vacuum : -20, action_move : -20, action_recharge : 10}
+reward_battery_full = {action_vacuum : -1, action_move : -1, action_recharge : -10}
+reward_battery_inter = {action_vacuum : -1, action_move : -1, action_recharge : 0}
+
+reward_wall_left = {action_vacuum : -1, action_recharge : -1}
+reward_wall_right = {action_vacuum : -1, action_recharge : -1}
+reward_wall_top = {action_vacuum : -1, action_recharge : -1}
+reward_wall_bottom = {action_vacuum : -1, action_recharge : -1}
+#TODO add reward for 4 directions
 
 #########################################
 
 class Simulator:
-  def launch():
-    hey = 'hey'
-    a = State
-    print(wallUp(a))
+  def launch(self):
+    s = State.State()
+    s.prettyPrint()
+    print(currentCellIsDirty(s))
+    print('bottom:'+str(wallBottom(s)))
+    print(reward_batterie_vide[action_move])
 
 #########################################
 
-s = Simulator
+s = Simulator()
 s.launch()
+
 
 
 
--- State.py
+++ State.py
@@ -2,23 +2,30 @@ import random
 
 class State:
 
-  battery  = 5              # Battery life. Between 0 & 5.
-  posRobot = [random.randint(0,2),random.randint(0,1)]          # x,y
-  posBase  = [posRobot[0],posRobot[1]]          # x,y
-  roomGrid = [[0, 0, 0],    # 0=clean, 1=dirty.
-              [0, 0, 0]]
+  # Setting the room size
+  sizeX = 3
+  sizeY = 2
+  # Picking some values for the robot and base initial position
+  randIntX = random.randint(0,sizeX-1)
+  randIntY = random.randint(0,sizeY-1)
+  # The components of a State:
+  battery  = 5                        # Battery life. Between 0 & 5.
+  posRobot = [randIntX,randIntY]      # (x,y)
+  posBase  = [randIntX,randIntY]      # (x,y)
+  roomGrid = [ [1]*sizeX ]*sizeY      # 0=clean, 1=dirty.
 
+  # Pretty-printing the state
   def prettyPrint(self):
-
+    # Note: only for 3*2 matrix
     matrix  = [[' ',' ',' '],[' ',' ',' ']]
     matrix[self.posRobot[1]][self.posRobot[0]] = 'r'
     matrix[self.posBase[1]][self.posBase[0]] = 'b'
-
-    print ('----- STATE -----')
+    matrix2 = [[' ' if i==0 else 'x' for i in self.roomGrid[0]],[' ' if i==0 else 'x' for i in self.roomGrid[1]]]
+    print('----- STATE -----')
     print('    -|'+self.battery*'o'+ (5-self.battery)*'' + '|+' )
-    print('      Room:')
-    print('     | | | |')
-    print('     | | | |')
-    print('      Robot:')
+    print('     Room:')
+    print('     |'+matrix2[0][0]+'|'+matrix2[0][1]+'|'+matrix2[0][2]+'|')
+    print('     |'+matrix2[1][0]+'|'+matrix2[1][1]+'|'+matrix2[1][2]+'|')
+    print('     Robot:')
     print('     |'+matrix[0][0]+'|'+matrix[0][1]+'|'+matrix[0][2]+'|')
     print('     |'+matrix[1][0]+'|'+matrix[1][1]+'|'+matrix[1][2]+'|')
