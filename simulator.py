0077e189d74538b3c00cb77c426b92e060a5631e
pauldes <paul.desigaud@insa-lyon.fr>
2017-10-04 18:37:11 +0200

    classe State

---
+++ simulator.py
@@ -0,0 +1,14 @@
+
+
+
+class State:
+
+  battery  = 5              # Battery life. Between 0 & 5.
+  posRobot = [0,0]          # x,y
+  posBase  = [0,0]          # x,y
+  roomGrid = [[0, 0, 0],    # 0=clean, 1=dirty.
+              [0, 0, 0]]
+
+
+
+
