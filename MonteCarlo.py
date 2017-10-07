843a724aaa19d946335892e3eb72f962b18529c7
Kian <kianjamali123@gmail.com>
2017-10-07 23:52:11 +0200

    Creation of an episode in MonteCarlo

--- MonteCarlo.py
+++ MonteCarlo.py
@@ -15,3 +15,16 @@ class MonteCarlo:
 
     simulator = Simulator.Simulator()
     r, list_possible_next_states = simulator.simulate(s, a, "Monte-Carlo")
+
+    # We choose randomly s2 in the possible new states
+    s2 = random.choice(list_possible_next_states)
+
+    a2 = p.random_policy()
+
+    r2, list_possible_next_states = simulator.simulate(s2, a2, "Monte-Carlo")
+
+    s3 = random.choice(list_possible_next_states)
+
+    a3 = p.random_policy()
+
+    r3, list_possible_next_states = simulator.simulate(s3, a3, "Monte-Carlo")
