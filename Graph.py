a177c812d0980a1ce6d1e9072cda111ae6779842
pauldes <paul.desigaud@insa-lyon.fr>
2017-10-12 18:36:51 +0200

    Graphics module

--- Graph.py
+++ Graph.py
@@ -1,10 +1,21 @@
 import pygal
 from MonteCarlo import MonteCarlo
+from DynamicProgramming import DynamicProgramming
 
 if __name__ == "__main__":
 
-  EPISODE_LENGTH = 100
+  EPISODE_LENGTH = 10
   INFINITE_LIMIT = 100
+
+  print('Computing Dynamic Programming...')
+  d_p = DynamicProgramming()
+  d_p_performance = d_p.main()
+  d_p_performances = []
+  d_p_performances.append(d_p_performance)
+  for i in range(0,INFINITE_LIMIT-2):
+    d_p_performances.append(None)
+  d_p_performances.append(d_p_performance)
+  print('done')
 
   print('Computing Monte-Carlo...')
   monte_carlo = MonteCarlo()
@@ -19,6 +30,7 @@ if __name__ == "__main__":
   graph.x_labels = map(str, range(0,INFINITE_LIMIT))
 
   graph.add('Monte-Carlo',monte_carlo_performances)
+  graph.add('Dynamic Programming',d_p_performances)
 
   graph.render_to_file('graph_rendering.svg')
   print('done')
